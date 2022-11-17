52f7acb3b49ef40ad1e77b8d462b79b0c504c69d
elsalvadordali <tloochich@icloud.com>
2022-11-17 16:55:20 -0500

    WIP plant selection

--- src/Plant.jsx
+++ src/Plant.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react"
+
 const Plant = ({plant}) => {
+    const event = JSON.parse(localStorage.getItem('event'))
+    const [selectedPlant, setPlant] = useState(event?.plant | null)
+
+    function updatePlant(plant) {
+        localStorage.setItem('event', JSON.stringify({...event, plant}))
+        console.log('plant', JSON.parse(localStorage.getItem('event')))
+    }
+
     return (
-        <div className="card" onClick={console.log('go to next page')}>
+        <div className="card" onClick={() => updatePlant(plant)}>
             <div className={plant + ' plant'}></div>
             <h4>{plant}</h4>
         </div>
--- src/SelectPlant.jsx
+++ src/SelectPlant.jsx
@@ -1,10 +1,11 @@
 import Plant from "./Plant"
 
 const SelectPlant = () => {
-    const plants = ['corn', 'carrot', 'couliflower', 'plum', 'eggplant', 'tulip', 'cabbage', 'weird', 'pumpkin', 'radish', 'lotus', 'purple', 'star fruit', 'cucumber']
+    const plants = ['corn', 'carrot', 'cauliflower', 'plum', 'eggplant', 'tulip', 'cabbage', 'wheat', 'pumpkin', 'radish', 'lotus', 'beet', 'star fruit', 'cucumber']
 
     return (
         <div>
+            <h5>{localStorage.getItem('event')?.plant}</h5>
             <h5>Select a tree</h5>
             {plants.map(p => <Plant key={p + '-card'} plant={p} />)}
         </div>

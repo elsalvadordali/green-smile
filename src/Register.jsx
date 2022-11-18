d07b3397143533499a6fe25307223cc6130932ae
Le Pham <phamtle724@gmail.com>
2022-11-17 21:39:23 -0800

    add try/catch to collect errors from firebase

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,29 +1,31 @@
-import { useState } from 'react'
-import { register } from './initFirebase'
-import './Forms.css'
+import { useState } from 'react';
+import { register } from './initFirebase';
+import './Forms.css';
 
 
-const Register = ({goNextPage}) => {
-    const [email, setEmail] = useState('')
-    const [pword, setPword] = useState('')
+const Register = ({ goNextPage }) => {
+    const [email, setEmail] = useState('');
+    const [pword, setPword] = useState('');
+    const [errMessage, setErrMessage] = useState(null);
 
     async function signUp(e) {
-        e.preventDefault()
-        const res = await register(email, pword)
-        goNextPage('writeEntry')
+        e.preventDefault();
+        const res = await register(email, pword);
+        console.log("RESULT IN REGISTER", res);
+        res.user.uid ? goNextPage('write') : setErrMessage(res.code)
     }
 
-    return ( 
+    return (
         <form onSubmit={(e) => signUp(e)} className='auth'>
             <h2>Register</h2>
             <label htmlFor='email' className='email-label'>Email</label>
-            <input type='text' id='email' onChange={(e) => setEmail(e.target.value)}/>
-            <label htmlFor='password' class='password-label'>Password</label>
-            <input type='text' id='password' onChange={(e) => setPword(e.target.value)}/>
+            <input type='text' id='email' onChange={(e) => setEmail(e.target.value)} />
+            <label htmlFor='password' className='password-label'>Password</label>
+            <input type='text' id='password' onChange={(e) => setPword(e.target.value)} />
 
             <button type='submit'>Register</button>
         </form>
-    )
-}
+    );
+};
 
-export default Register
+export default Register;

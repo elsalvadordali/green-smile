5b6ce3442263f45fa8d2cc9e41612bc063b9bc3e
Ash Moreno <ashterfuentes@gmail.com>
2022-11-18 20:34:26 -0600

    added css for write entry

--- src/WriteEntry.jsx
+++ src/WriteEntry.jsx
@@ -1,49 +1,54 @@
 import { useState } from "react";
+import "./WriteEntry.css";
 
 const WriteEntry = ({ promptNumber, goNextPage }) => {
-    let monthEntry = {};
-    let month = new Date().getMonth().toString();
-    let date = new Date().getDate().toString();
+  let monthEntry = {};
+  let month = new Date().getMonth().toString();
+  let date = new Date().getDate().toString();
 
-    monthEntry[month] = {
-        [date]: {
-            promptNumber: 0,
-            entry: "",
-        }
+  monthEntry[month] = {
+    [date]: {
+      promptNumber: 0,
+      entry: "",
+    },
+  };
+  let lsEntry = JSON.parse(localStorage.getItem("entry"));
+  const [journal, setJournal] = useState(lsEntry ? lsEntry : { ...monthEntry });
+
+  localStorage.setItem("entry", JSON.stringify(journal));
+  const prompts = [
+    `Today I'm proud of...`,
+    `I'm thankful for...`,
+    `I honored myself by...`,
+    `I grew today by...`,
+    `Today was a great day because...`,
+    `Today I smiled because...`,
+    `I'll never forget today because...`,
+  ];
+  function submit(e) {
+    e.preventDefault();
+    goNextPage("select");
+  }
+  function updateJournalEntry(text) {
+    let currentEntry = journal[month][date];
+    currentEntry.entry = text;
+    let newEntry = {
+      ...journal,
+      [month]: { [date]: currentEntry },
     };
-    let lsEntry = JSON.parse(localStorage.getItem('entry'))
-    const [journal, setJournal] = useState(lsEntry ?
-        lsEntry : { ...monthEntry });
-
-    localStorage.setItem('entry', JSON.stringify(journal));
-    const prompts = [
-        `Today I'm proud of...`,
-        `I'm thankful for...`,
-        `I honored myself by...`,
-        `I grew today by...`,
-        `Today was a great day because...`,
-        `Today I smiled because...`,
-        `I'll never forget today because...`
-    ];
-    function submit(e) {
-        e.preventDefault()
-        goNextPage('select')
-    }
-    function updateJournalEntry(text) {
-        let currentEntry = journal[month][date]
-        currentEntry.entry = text
-        let newEntry = {
-            ...journal, [month]: { [date]: currentEntry }
-        }
-        setJournal(newEntry);
-    }
-    return (
-        <form onSubmit={(e) => submit(e)}>
-            <h3>{prompts[promptNumber]}</h3>
-            <textarea value={journal[month][date].entry} onChange={(e) => updateJournalEntry(e.target.value)} />
-            <button>Give Thanks!</button>
-        </form>
-    );
+    setJournal(newEntry);
+  }
+  return (
+    <form onSubmit={(e) => submit(e)}>
+      <h3 className="gratitude-prompts">{prompts[promptNumber]}</h3>
+      <textarea
+        className="gratitude-entry"
+        value={journal[month][date].entry}
+        onChange={(e) => updateJournalEntry(e.target.value)}
+      />
+      <button className="gratitude-button">Give Thanks!</button>
+    </form>
+  );
 };
 
 export default WriteEntry;

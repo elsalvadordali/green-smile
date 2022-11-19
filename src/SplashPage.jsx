45bc2f4c24503bf613a0bc77df93d5bdb3171d38
elsalvadordali <tloochich@icloud.com>
2022-11-19 12:23:23 -0500

    logo update

--- src/SplashPage.jsx
+++ src/SplashPage.jsx
@@ -16,7 +16,7 @@ const SplashPage = ({ goNextPage, updateUserId }) => {
       <div className='splash'>
         <h1>Happy<span>Garden</span></h1>
         <p>Visualize the effect of mindfulness by growing plants</p>
-        <div className='splash-img'><div className='plant plum-3'></div></div>
+        <div className='splash-img'><div className='plant logo'></div></div>
       </div>
       {showLogin && <Login goNextPage={goNextPage} updateUserId={updateUserId} toggleLogin={toggleLogin} />}
       {showLogin == false && <Register goNextPage={goNextPage} updateUserId={updateUserId} toggleLogin={toggleLogin} />}
